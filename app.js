a6f759f8d3a96a2f7c87efe7a3746f4f4b0a1ac4
jagadum <jarugumallinuthan@gmail.com>
2024-04-12 14:41:12 -0500

    Update app.js

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ var pickRouter = require('./routes/pick');
 var resourceRouter = require('./routes/resource');
 var app = express();
 
-// view engine setup
+
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
@@ -29,18 +29,15 @@ app.use('/dress', dressRouter);
 app.use('/grid', gridRouter); 
 app.use('/pick', pickRouter); 
 app.use('/resource', resourceRouter); 
-// catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
 });
 
-// error handler
+
 app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // render the error page
   res.status(err.status || 500);
   res.render('error');
 });
@@ -51,17 +48,14 @@ require('dotenv').config();
 const connectionString = process.env.MONGO_CON
 mongoose = require('mongoose');
 mongoose.connect(connectionString);
-//Get the default connection
 var db = mongoose.connection;
-//Bind connection to error event
 db.on('error', console.error.bind(console, 'MongoDB connection error:'));
 db.once("open", function(){
 console.log("Connection to DB succeeded")});
 
-// We can seed the collection if needed on
+
 
 async function recreateDB(){
-// Delete everything
 await dress.deleteMany();
 
 let instance1 = new dress({dress_type: 'Casual', dress_size:'Medium', dress_price: 30});
@@ -90,7 +84,7 @@ console.error(err)
 let reseed = true;
 if (reseed) {recreateDB();}
 
-//lab-13
+//lab-13//nj
 var passport = require('passport');
 var LocalStrategy = require('passport-local').Strategy;
 passport.use(new LocalStrategy(
@@ -120,9 +114,6 @@ passport.use(new LocalStrategy(
     app.use(passport.initialize());
     app.use(passport.session());
 
-// passport config
-// Use the existing connection
-// The Account model
 var Account =require('./models/account');
 passport.use(new LocalStrategy(Account.authenticate()));
 passport.serializeUser(Account.serializeUser());
