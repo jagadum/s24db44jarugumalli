144dbd769c7417ba3f9f49eab8d4aa45a440e77e
jagadum <jarugumallinuthan@gmail.com>
2024-04-12 19:34:06 -0500

    Update app.js

--- app.js
+++ app.js
@@ -3,88 +3,7 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-var dress = require("./models/dress");
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
-var dressRouter = require('./routes/dress');
-var gridRouter = require('./routes/grid');
-var pickRouter = require('./routes/pick');
-var resourceRouter = require('./routes/resource');
-var app = express();
-
-
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'pug');
-
-app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
-
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
-app.use('/dress', dressRouter); 
-app.use('/grid', gridRouter); 
-app.use('/pick', pickRouter); 
-app.use('/resource', resourceRouter); 
-app.use(function(req, res, next) {
-  next(createError(404));
-});
-
-
-app.use(function(err, req, res, next) {
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  res.status(err.status || 500);
-  res.render('error');
-});
-
-module.exports = app;
-
-require('dotenv').config();
-const connectionString = process.env.MONGO_CON
-mongoose = require('mongoose');
-mongoose.connect(connectionString);
-var db = mongoose.connection;
-db.on('error', console.error.bind(console, 'MongoDB connection error:'));
-db.once("open", function(){
-console.log("Connection to DB succeeded")});
-
-
-
-async function recreateDB(){
-await dress.deleteMany();
-
-let instance1 = new dress({dress_type: 'Casual', dress_size:'Medium', dress_price: 30});
-instance1.save().then(doc=>{
-console.log("First object saved")}
-).catch(err=>{
-console.error(err)
-});
-
-
-let instance2 = new dress({dress_type: 'Formal', dress_size:'Large', dress_price: 50});
-instance2.save().then(doc=>{
-console.log("Second object saved")}
-).catch(err=>{
-console.error(err)
-});
-
-
-let instance3 = new dress({dress_type: 'Party', dress_size:'Small', dress_price: 40});
-instance3.save().then(doc=>{
-console.log("Third object saved")}
-).catch(err=>{
-console.error(err)
-});
-}
-let reseed = true;
-if (reseed) {recreateDB();}
-
-//lab-13//nj
 var passport = require('passport');
 var LocalStrategy = require('passport-local').Strategy;
 passport.use(new LocalStrategy(
@@ -106,15 +25,101 @@ passport.use(new LocalStrategy(
   })
   )
 
-  app.use(require('express-session')({
-    secret: 'keyboard cat',
-    resave: false,
-    saveUninitialized: false
-    }));
-    app.use(passport.initialize());
-    app.use(passport.session());
+var indexRouter = require('./routes/index');
+var usersRouter = require('./routes/users');
+var dressRouter = require('./routes/dress');
+var gridrRouter = require('./routes/grid');
+var pickRouter = require('./routes/pick');
+var dress = require("./models/dress");
+var resourceRouter = require("./routes/resource");
+var app = express();
 
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'pug');
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+
+app.use(require('express-session')({
+  secret: 'keyboard cat',
+  resave: false,
+  saveUninitialized: false
+  }));
+  app.use(passport.initialize());
+  app.use(passport.session());
+  
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+app.use('/dress', dressRouter);
+app.use('/grid',gridrRouter);
+app.use('/pick',pickRouter);
+app.use('/resource',resourceRouter);
+
+// passport config
+// Use the existing connection
+// The Account model
 var Account =require('./models/account');
 passport.use(new LocalStrategy(Account.authenticate()));
 passport.serializeUser(Account.serializeUser());
 passport.deserializeUser(Account.deserializeUser());
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+module.exports = app;
+
+
+require('dotenv').config();
+const connectionString = process.env.MONGO_CON
+mongoose = require('mongoose');
+mongoose.connect(connectionString);
+//Get the default connection
+var db = mongoose.connection;
+//Bind connection to error event
+db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+db.once("open", function(){
+console.log("Connection to DB succeeded")});
+
+// We can seed the collection if needed on
+
+async function recreateDB(){
+// Delete everything
+let instance1 = new dress({dress_type: 'Casual', dress_size:'Medium', dress_price: 30});
+instance1.save().then(doc=>{
+console.log("First object saved")}
+).catch(err=>{
+console.error(err)
+});
+let instance2 = new dress({dress_type: 'Formal', dress_size:'Large', dress_price: 50});
+instance2.save().then(doc=>{
+console.log("Second object saved")}
+).catch(err=>{
+console.error(err)
+});
+let instance3 = new dress({dress_type: 'Party', dress_size:'Small', dress_price: 40});
+instance3.save().then(doc=>{
+console.log("Third object saved")}
+).catch(err=>{
+console.error(err)
+});
+}
+let reseed = true;
+if (reseed) {recreateDB();}
