4f9c650e37657ceee31b24e9da4955aa8284cd00
jagadum <jarugumallinuthan@gmail.com>
2024-04-14 19:12:06 -0500

    Update dress.js

--- models/dress.js
+++ models/dress.js
@@ -3,15 +3,14 @@ const mongoose = require('mongoose');
 const dressSchema = mongoose.Schema({
   dress_type: {
     type: String,
-    required: [true, 'Dress type is required'],
-    maxlength: [20, 'Dress type must be at most 20 characters long']
+    minlength:[4],
+    maxlength: [20]
   },
   dress_size: String,
   dress_price: {
     type: Number,
-    min: [0, 'Dress Price must be a positive number'],
-    max: [1000, 'Dress Price cannot exceed $1000'],
-    required: [true, 'Price is required']
+    min: [0],
+    max: [1000],
   }
 });
 
