3e178a5dc1bfcbbe1678036a5da911703871acd7
jagadum <jarugumallinuthan@gmail.com>
2024-04-16 14:44:07 -0500

    added

--- models/dress.js
+++ models/dress.js
@@ -3,14 +3,14 @@ const mongoose = require('mongoose');
 const dressSchema = mongoose.Schema({
   dress_type: {
     type: String,
-    minlength:[4],
-    maxlength: [20]
+    minlength:4,
+    maxlength: 20
   },
   dress_size: String,
   dress_price: {
     type: Number,
-    min: [0],
-    max: [1000],
+    min: 0,
+    max: 1000,
   }
 });
 
